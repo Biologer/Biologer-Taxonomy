45f373ff38d012da52df85d0cfad2ae71844f2e6
Nikola Vasić <16414020+nvasic88@users.noreply.github.com>
2023-05-17 18:36:35 +0200

    Language update

--- resources/lang/sr/labels.php
+++ resources/lang/sr/labels.php
@@ -75,9 +75,10 @@ return [
         'red_lists' => 'Црвене листе',
         'add_red_list' => 'Додај црвену листу',
         'search_for_taxon' => 'Тражи таксон...',
+        'yes' => 'Да',
+        'no' => 'Не',
 
         'include_lower_taxa' => 'Укључујући ниже таксоне',
-
 
         'atlas_codes' => 'Атлас кодови',
         'uses_atlas_codes' => 'Користи Атлас кодове',
