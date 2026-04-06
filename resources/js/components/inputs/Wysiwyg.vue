191f46f1808bd96e386357575c34785b1b10027d
Nikola Vasić <16414020+nvasic88@users.noreply.github.com>
2026-04-06 18:27:26 +0200

    Trix.config.toolbar is a read-only property in new version of Trix

--- resources/js/components/inputs/Wysiwyg.vue
+++ resources/js/components/inputs/Wysiwyg.vue
@@ -14,13 +14,12 @@ Vue.config.ignoredElements = ['trix-editor']
 
 let { lang } = Trix.config
 
-Trix.config.toolbar = {
-  getDefaultHTML: () => `
+Trix.config.toolbar.getDefaultHTML = () => `
     <div class="trix-button-row">
       <span class="trix-button-group trix-button-group--text-tools" data-trix-button-group="text-tools">
         <button type="button" class="trix-button trix-button--icon trix-button--icon-bold" data-trix-attribute="bold" data-trix-key="b" title="${lang.bold}" tabindex="-1">${lang.bold}</button>
         <button type="button" class="trix-button trix-button--icon trix-button--icon-italic" data-trix-attribute="italic" data-trix-key="i" title="${lang.italic}" tabindex="-1">${lang.italic}</button>
-        <button type="button" class="trix-button trix-button--icon trix-button--icon-strike" data-trix-attribute="strike" title="#{lang.strike}" tabindex="-1">${lang.strike}</button>
+        <button type="button" class="trix-button trix-button--icon trix-button--icon-strike" data-trix-attribute="strike" title="${lang.strike}" tabindex="-1">${lang.strike}</button>
         <button type="button" class="trix-button trix-button--icon trix-button--icon-link" data-trix-attribute="href" data-trix-action="link" data-trix-key="k" title="${lang.link}" tabindex="-1">${lang.link}</button>
       </span>
       <span class="trix-button-group trix-button-group--history-tools" data-trix-button-group="history-tools">
@@ -39,8 +38,7 @@ Trix.config.toolbar = {
         </div>
       </div>
     </div>
-  `
-}
+  `;
 
 function makeid() {
   let text = ""
